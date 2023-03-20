16dd975dfd7cc46b144a8a6a7db71e96efb0937b
Yadu krishnan M <109993760+yaduKrishna123@users.noreply.github.com>
2023-03-20 20:52:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const jsonserver= require('json-server')
+const jsonServer= require('json-server')
 const server=jsonServer.create()
 const router=jsonServer.router('db.json')
 const middlewires=jsonServer.defaults()
